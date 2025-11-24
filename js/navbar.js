b9e8d09f4d4b4b0317aab5b265f1809d265a30f1
StheshSphere <stheshsphere@gmail.com>
2025-11-24 21:24:12 +0200

    updates :)

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,17 @@
-navbar.js
+// fetch("components/navbar.html")
+//     .then(res => res.text())
+//     .then(data => {
+//         document.innerHTML("navbar").innerHTML = data;
+//     });
+
+// fetch("components/footer.html")
+//     .then(res => res.text())
+//     .then(data => {
+//         document.getElementById("footer").innerHTML = data;
+//     });
+const navbarContainer = document.querySelector('.navbar');
+fetch("components/navbar.html")
+    .then(res => res.text())
+    .then(data => {
+        navbarContainer.innerHTML = data;
+    });
