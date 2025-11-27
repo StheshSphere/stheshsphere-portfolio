77e1e4d82faab7feae833768ec43856386c93030
StheshSphere <stheshsphere@gmail.com>
2025-11-28 01:17:04 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -8,3 +8,19 @@ fetch("components/footer.html")
     .then(data => {
         document.getElementById('footer').innerHTML = data;
     });
+// DROPDOWN TOGGLE
+document.addEventListener("click", () => {
+    const dropdownBtn = document.querySelector(".dropdown-btn");
+    const mobileNav = document.getElementById("mobile-nav-links");
+
+    dropdownBtn.addEventListener("click", function (e) {
+        e.stopPropagation();    // prevents closing instantly
+        mobileNav.style.display =
+            mobileNav.style.display === "flex" ? "none" : "flex";
+    });
+
+    // Clicking outside closes menu
+    document.addEventListener("click", function () {
+        mobileNav.style.display = "none";
+    });
+});
